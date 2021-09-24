9afb4eb9924968e35823c08a60e66763ef2f4442
remus432 <brosdev1@gmail.com>
2021-09-24 16:00:34 +0300

    first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 const dbURI = "mongodb+srv://remus432:bunasearabucuresti1@nodeblog.ugtkl.mongodb.net/node-todo?retryWrites=true&w=majority"
 mongoose.connect(dbURI)
 
-app.use(cors({ origin: "https://todo-app-pi-roan.vercel.app"}))
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
